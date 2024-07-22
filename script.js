c87d8bba27bd1e74def26b49fbaa5efdaee8a089
gdsonicpython <109347539+gdsonicpython@users.noreply.github.com>
2024-07-22 14:57:10 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ function floatToString(float) {
     return float.toFixed(2).toString();
 }
 
-function algorithm(n) {
-    return (n / 6.25) + (n / (n / n));
+function algorithmreadable(n) {
+    return (n / 6.25) + n);
+}
+
+function algorithmfast(n) {
+    return n*1.16;
 }
 
 document.getElementById('number').addEventListener('input', function() {
     let floatValue = stringToFloat(this.value);
     if (!isNaN(floatValue)) {
-        let result = algorithm(floatValue);
+        let result = algorithmfast(floatValue);
         document.getElementById('text').innerText = floatToString(result);
     } else {
         document.getElementById('text').innerText = '';
